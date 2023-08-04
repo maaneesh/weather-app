eee7eafc89818fe0c47694c5557006f909e7a36c
Manish Chaudhari <chaudharimanish07@gmail.com>
2023-08-04 09:11:40 -0400

    K to F conversion

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Weather from './components/Weather';
+
+
+function App() {
+  const APIKEY = 'ca83e9f1d7f479142e8f0f3e6bd8e16e';
+
+  return (
+    <>
+    <Weather/>
+    </>
+    
+  )
+}
+
+export default App
