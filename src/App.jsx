6e20ac7bb39fb3dc8927f231b0c56d0f7dd5c49c
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-08-09 15:43:27 -0400

    added a form to search location

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,21 @@ function App() {
           <h1>Hello</h1>
         </div>
         <div className="px-1">
-          <h1 className="nav flex al">Search</h1>
+          <label class="input input-bordered flex items-center gap-2">
+            <input type="text" class="grow" placeholder="Location" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              class="h-4 w-4 opacity-70"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </label>
         </div>
       </div>
       <main className="h-dvh">
@@ -28,13 +42,9 @@ function App() {
       </main>
       <aside></aside>
 
-      <div className="footer  bg-sky-500  text-white  hover:text-red-500 px-2 py-2 text-4xl font-bold">
-        <div>
-          <h2>About</h2>
-        </div>
-        <div>
-          <h2>Careers</h2>
-        </div>
+      <div className="footer  bg-sky-500  flex-1 text-white  hover:text-red-500 px-2 py-2 text-4xl font-bold">
+        <h2>About</h2>
+        <h2>Careers</h2>
       </div>
     </>
   );
