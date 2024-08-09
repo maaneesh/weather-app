7991903cf66f46b6dc319f980425dfce5f537932
Manish Chaudhari <chaudharimanish07@gmail.com>
2024-08-09 15:02:51 -0400

    installed DaisyUI dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="bg-sky-500 px-2 py-2 text-4xl font-bold">
       <h1>Hello</h1>
-    </>
+    </div>
   );
 }
 
